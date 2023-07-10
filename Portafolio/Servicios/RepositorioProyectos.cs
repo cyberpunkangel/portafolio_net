f3d5845efce18193b52e8ce418917e74e88431d9
Angel López <149angel@hotmail.com>
2023-07-10 13:12:36 -0600

    Primer commit

---
+++ Portafolio/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,47 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Servicios
+{
+    public interface IRepositorioProyectos
+    {
+        List<Proyecto> ObtenerProyectos();
+    }
+
+    public class RepositorioProyectos: IRepositorioProyectos
+    {
+        public List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>() {
+                new Proyecto
+            {
+                Titulo = "Slot 1",
+                Descripcion = "Descripción slot 1",
+                Link = "https://www.google.com/",
+                LinkRepositorio = "https://google.com",
+                ImagenURL = "/imagenes/amazon.png"
+            },
+            //    new Proyecto
+            //{
+            //    Titulo = "Ejemplo1",
+            //    Descripcion = "Página de noticias en React",
+            //    Link = "https://nytimes.com",
+            //    ImagenURL = "/imagenes/nyt.png"
+            //},
+            //    new Proyecto
+            //{
+            //    Titulo = "Ejemplo2",
+            //    Descripcion = "Red social para compartir en comunidades",
+            //    Link = "https://reddit.com",
+            //    ImagenURL = "/imagenes/reddit.png"
+            //},
+            //    new Proyecto
+            //{
+            //    Titulo = "Ejemplo3",
+            //    Descripcion = "Tienda en línea para comprar videojuegos",
+            //    Link = "https://store.steampowered.com",
+            //    ImagenURL = "/imagenes/steam.png"
+            //},
+            };
+        }
+    }
+}
---
+++ Portafolio/Servicios/ServicioEmailSendGrid.cs
@@ -0,0 +1,64 @@
+﻿using Portafolio.Models;
+using System.Net;
+using System.Net.Mail;
+
+namespace Portafolio.Servicios
+{
+    public interface IServicioEmail
+    {
+        Task Enviar(ContactoViewModel contacto);
+    }
+
+    public class ServicioEmailSmtp : IServicioEmail
+    {
+        private readonly IConfiguration configuration;
+
+        public ServicioEmailSmtp(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public async Task Enviar(ContactoViewModel contacto)
+        {
+            var email = configuration.GetValue<string>("SMTP_FROM");
+            var nombre = configuration.GetValue<string>("SMTP_NOMBRE");
+            var password = configuration.GetValue<string>("SMTP_PASSWORD");
+            var host = "smtp.gmail.com"; // Host SMTP de Gmail
+            var port = 587; // Puerto para SMTP de Gmail
+            var cc = configuration.GetValue<string>("SMTP_CC"); // Dirección de correo electrónico para el campo CC
+
+            if (string.IsNullOrEmpty(email))
+                throw new ArgumentNullException(nameof(email), "La dirección de correo electrónico 'from' no puede ser nula o vacía.");
+
+            if (string.IsNullOrEmpty(password))
+                throw new ArgumentNullException(nameof(password), "La contraseña de correo electrónico no puede ser nula o vacía.");
+
+            var from = new MailAddress(email, nombre);
+            var subject = $"El cliente {contacto.Email} quiere contactarte";
+            var to = new MailAddress(email, nombre);
+            var ccAddress = new MailAddress(cc); // Crear el objeto MailAddress para el campo CC
+            var mensajeTextoPlano = contacto.Mensaje;
+            var contenidoHtml = @$"De: {contacto.Nombre} - 
+Email: {contacto.Email}
+Mensaje: {contacto.Mensaje}";
+
+            using (var client = new SmtpClient(host, port))
+            {
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(email, password);
+
+                using (var message = new MailMessage(from, to))
+                {
+                    message.Subject = subject;
+                    message.Body = contenidoHtml;
+                    message.IsBodyHtml = true;
+
+                    message.CC.Add(ccAddress); // Agregar la dirección de correo electrónico al campo CC
+
+                    await client.SendMailAsync(message);
+                }
+            }
+        }
+    }
+}
